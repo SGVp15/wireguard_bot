7187f8bb3129f73de60644eb9ce7fc028de70545
SGVp15 <phosphorusp15@mail.ru>
2024-10-15 12:48:24 +0300

    main

--- Telegram/handlers/user.py
+++ Telegram/handlers/user.py
@@ -3,10 +3,12 @@ from typing import Dict, Any
 from aiogram import F
 from aiogram import types
 from aiogram.fsm.context import FSMContext
+from aiogram.types import FSInputFile
 
 from Telegram import keybords
 from Telegram.Call_Back_Data import CallBackData
 from Telegram.config import ADMIN_ID
+from Telegram.keybords.inline import main_menu
 from Telegram.main import dp, bot
 from Telegram.states.Form import Form
 from WG import wg
@@ -25,6 +27,7 @@ async def create_user(callback_query: types.callback_query, state: FSMContext):
     text, file = wg.create_user(user)
     log.info('create_user {user}')
     await state.clear()
-    with open(file, "rb") as f:
-        await bot.send_document(chat_id=callback_query.from_user.id, document=f,
-                                reply_markup=keybords.inline.main_menu)
+
+    file = FSInputFile(file, filename=user)
+    await bot.send_document(chat_id=callback_query.from_user.id, document=file, reply_markup=main_menu)
+
