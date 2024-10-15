0b691a41a21c42ad169333f8aacd276c43c63c90
SGVp15 <phosphorusp15@mail.ru>
2024-10-15 11:39:38 +0300

    main

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from dotenv import dotenv_values, find_dotenv
 
-config = dotenv_values(config = dotenv_values('./.env')())
+config = dotenv_values(find_dotenv())
 
 SERVER_IP = config['SERVER_IP']
 
