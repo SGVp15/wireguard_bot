7d9df3570606fa5a528023133da2439d090168bc
SGVp15 <phosphorusp15@mail.ru>
2024-10-15 12:14:30 +0300

    wg

--- Telegram/keybords/menu_admin.py
+++ Telegram/keybords/menu_admin.py
@@ -5,7 +5,7 @@ from Telegram.Call_Back_Data import CallBackData
 k_menu_admin = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='<< Назад', callback_data=CallBackData.menu_main), ],
     [InlineKeyboardButton(text='🌟 Перезагрузить службу WG >>', callback_data=CallBackData.menu_restart_service_wg), ],
-    [InlineKeyboardButton(text='🌟 Перезагрузить сервер WG >>', callback_data=CallBackData.menu_reboot_server), ],
+    [InlineKeyboardButton(text='💀 Перезагрузить сервер WG >>', callback_data=CallBackData.menu_reboot_server), ],
 ])
 
 k_menu_restart_service = InlineKeyboardMarkup(inline_keyboard=[
