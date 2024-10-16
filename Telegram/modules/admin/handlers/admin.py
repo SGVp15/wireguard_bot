e138175048c4e26a46b4c577eebed118fc339849
SGVp15 <phosphorusp15@mail.ru>
2024-10-16 14:46:13 +0300

    refactoring to module

--- Telegram/modules/admin/handlers/admin.py
+++ Telegram/modules/admin/handlers/admin.py
@@ -5,22 +5,19 @@ from aiogram import types
 from aiogram.fsm.context import FSMContext
 from aiogram.types import FSInputFile
 
-from Telegram.keyboards.menu_main import k_main_menu
-from Telegram.config import ADMIN_ID
 from Telegram.Call_Back_Data import CallBackData
+from Telegram.config import ADMIN_ID
+from Telegram.keyboards.menu_main import k_main_menu
 from Telegram.main import dp, bot
-
 from Telegram.modules.admin.keyboards.menu_admin import k_menu_admin
-from Telegram.modules.admin.states.mashine_state import AdminState
-
-from config import WG_CONF, WG_DUMP
+from config import WG_CONF, WG_DUMP, SYSTEM_LOG
 from wireguard.wireguard_class import WIREGUARD as wg
 
 
-@dp.callback_query(AdminState.menu_restart_service_wg,
-                   F.data.in_({CallBackData.restart_service_wg_ok, })
-                   & F.from_user.id.in_({*ADMIN_ID, })
-                   )
+@dp.callback_query(
+    F.data.in_({CallBackData.restart_service_wg_ok, })
+    & F.from_user.id.in_({*ADMIN_ID, })
+)
 async def restart_service(callback_query: types.callback_query, state: FSMContext):
     await state.clear()
     wg.restart_service()
@@ -31,10 +28,10 @@ async def restart_service(callback_query: types.callback_query, state: FSMContex
     )
 
 
-@dp.callback_query(AdminState.menu_restart_service_wg,
-                   F.data.in_({CallBackData.reboot_server_ok, })
-                   & F.from_user.id.in_({*ADMIN_ID, })
-                   )
+@dp.callback_query(
+    F.data.in_({CallBackData.reboot_server_ok, })
+    & F.from_user.id.in_({*ADMIN_ID, })
+)
 async def restart_service(callback_query: types.callback_query, state: FSMContext):
     await state.clear()
     wg.reboot_server()
@@ -45,20 +42,32 @@ async def restart_service(callback_query: types.callback_query, state: FSMContex
     )
 
 
-@dp.callback_query(AdminState.admin_menu,
-                   F.data.in_({CallBackData.download_wg_conf, })
-                   & F.from_user.id.in_({*ADMIN_ID, })
-                   )
-async def download_logs(callback_query: types.callback_query, state: FSMContext):
+@dp.callback_query(
+    F.data.in_({CallBackData.download_wg_conf, })
+    & F.from_user.id.in_({*ADMIN_ID, })
+)
+async def download_wg_conf(callback_query: types.callback_query, state: FSMContext):
     file = WG_CONF
     filename = f'wg0.conf'
     await send_document(file, filename, callback_query.from_user.id)
 
 
-@dp.callback_query(AdminState.admin_menu,
-                   F.data.in_({CallBackData.download_wg_dump, })
-                   & F.from_user.id.in_({*ADMIN_ID, })
-                   )
+@dp.callback_query(
+    # AdminState.admin_menu,
+    F.data.in_({CallBackData.download_logs, })
+    & F.from_user.id.in_({*ADMIN_ID, })
+)
+async def download_logs(callback_query: types.callback_query, state: FSMContext):
+    file = SYSTEM_LOG
+    filename = f'log.log'
+    print(state)
+    await send_document(file, filename, callback_query.from_user.id)
+
+
+@dp.callback_query(
+    F.data.in_({CallBackData.download_wg_dump, })
+    & F.from_user.id.in_({*ADMIN_ID, })
+)
 async def download_wg_dump(callback_query: types.callback_query, state: FSMContext):
     wg.get_dump()
     file = WG_DUMP
