19a702c68450823e6529b65aa1a9a5888e636a76
SGVp15 <phosphorusp15@mail.ru>
2024-10-16 18:31:37 +0300

    router

--- Telegram/modules/admin/keyboards/menu_admin.py
+++ Telegram/modules/admin/keyboards/menu_admin.py
@@ -3,22 +3,25 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from Telegram.Call_Back_Data import CallBackData
 
 k_menu_admin = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='<< Назад', callback_data=CallBackData.menu_main), ],
-    [InlineKeyboardButton(text='🌟 Перезагрузить службу wireguard', callback_data=CallBackData.menu_restart_service_wg), ],
+    [InlineKeyboardButton(text='🌟 Перезагрузить службу wireguard',
+                          callback_data=CallBackData.menu_restart_service_wg), ],
     [InlineKeyboardButton(text='💀 Перезагрузить сервер', callback_data=CallBackData.menu_reboot_server), ],
     [InlineKeyboardButton(text='📒 Скачать логи', callback_data=CallBackData.download_logs), ],
     [InlineKeyboardButton(text='📒 Скачать WG_CONF', callback_data=CallBackData.download_wg_conf), ],
     [InlineKeyboardButton(text='📒 Скачать WG_DUMP', callback_data=CallBackData.download_wg_dump), ],
+    [InlineKeyboardButton(text='🔙 Назад', callback_data=CallBackData.menu_main), ]
 ])
 
 k_menu_restart_service = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='<< Назад', callback_data=CallBackData.menu_admin), ],
     [InlineKeyboardButton(text='Все верно перезагрузить службу wireguard',
                           callback_data=CallBackData.restart_service_wg_ok), ],
+    [InlineKeyboardButton(text='🔙 Назад', callback_data=CallBackData.menu_admin), ],
+
 ])
 
 k_menu_reboot_server = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='<< Назад', callback_data=CallBackData.menu_admin), ],
     [InlineKeyboardButton(text='Все верно перезагрузить Server',
                           callback_data=CallBackData.reboot_server_ok), ],
+    [InlineKeyboardButton(text='🔙 Назад', callback_data=CallBackData.menu_admin), ],
+
 ])
--- Telegram/modules/user/keyboards/menu_user.py
+++ Telegram/modules/user/keyboards/menu_user.py
@@ -3,15 +3,17 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from Telegram.Call_Back_Data import CallBackData
 
 k_menu_users = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='<< Назад', callback_data=CallBackData.menu_main), ],
     [InlineKeyboardButton(text='🌟 Создать пользователя >>', callback_data=CallBackData.menu_create_user), ],
+    [InlineKeyboardButton(text='🔙 Назад', callback_data=CallBackData.menu_main), ],
+
 ])
 
 k_menu_user_create = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='<< Назад', callback_data=CallBackData.menu_users), ],
     [InlineKeyboardButton(text='Все верно :) создать', callback_data=CallBackData.create_user_ok), ],
+    [InlineKeyboardButton(text='🔙 Назад', callback_data=CallBackData.menu_users), ],
+
 ])
 
 k_back_to_menu_users = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='<< Назад', callback_data=CallBackData.menu_users), ]
+    [InlineKeyboardButton(text='🔙 Назад', callback_data=CallBackData.menu_users), ]
 ])
