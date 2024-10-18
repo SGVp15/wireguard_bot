943c70cead951a19a6b14281df8fb250dab579f4
SGVp15 <phosphorusp15@mail.ru>
2024-10-18 13:13:37 +0300

    git

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-from xml.sax.handler import version
-
 from dotenv import dotenv_values, find_dotenv
 
 config = dotenv_values(find_dotenv())
@@ -11,6 +9,5 @@ WG_CONF = '/etc/wireguard/wg0.conf'
 WG_DUMP = './wg_dump.txt'
 
 PATTERN_USER = r'\s*([А-Я][а-я]+\s+[А-Я][а-я]+)\s*'
-
 
 VERSION = '1.3'
