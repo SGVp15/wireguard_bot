9db9e1da189dc5feb12ae155b77f71fc3e001487
SGVp15 <phosphorusp15@mail.ru>
2025-01-14 02:02:36 +0300

    log

--- Telegram/modules/admin/handlers/files.py
+++ Telegram/modules/admin/handlers/files.py
@@ -8,7 +8,6 @@ from aiogram.types import FSInputFile, CallbackQuery
 from Telegram.Call_Back_Data import CallBackData
 from Telegram.config import ADMIN_ID
 from Telegram.loader import bot
-from Telegram.modules.admin.handlers.admin import router
 from Telegram.modules.admin.keyboards.menu_admin import k_menu_admin
 from Telegram.modules.admin.keyboards.menu_files import get_config_list_files_keyboard
 from config import PATH_QR, PATH_CONFIG
