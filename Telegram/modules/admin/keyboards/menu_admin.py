9fae002f3e3627039971c2aee913075548dada2e
SGVp15 <phosphorusp15@mail.ru>
2025-01-14 01:49:02 +0300

    log

--- Telegram/modules/admin/keyboards/menu_admin.py
+++ Telegram/modules/admin/keyboards/menu_admin.py
@@ -11,7 +11,7 @@ k_menu_admin = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='📒 Скачать WG_DUMP', callback_data=CallBackData.download_wg_dump), ],
     [InlineKeyboardButton(text='♻️ Обновить Bot', callback_data=CallBackData.update_bot), ],
     [InlineKeyboardButton(text='❌ Удалить логи', callback_data=CallBackData.clear_log), ],
-    [InlineKeyboardButton(text=' Показать версию', callback_data=CallBackData.show_version), ],
+    [InlineKeyboardButton(text='? Показать версию', callback_data=CallBackData.show_version), ],
     [InlineKeyboardButton(text='🔖 Показать Configs', callback_data=CallBackData.show_config_files), ],
     [InlineKeyboardButton(text='🔙 Назад', callback_data=CallBackData.menu_main), ]
 ])
