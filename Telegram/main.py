ec098c7906277d93cd1e44bc2fe97e7281eb82c4
SGVp15 <phosphorusp15@mail.ru>
2025-01-14 02:03:56 +0300

    log

--- Telegram/main.py
+++ Telegram/main.py
@@ -2,10 +2,12 @@ from Telegram.handlers.echo import router as echo_router
 from Telegram.handlers.start import router as start_router
 from Telegram.loader import bot, router, dp
 from Telegram.modules.admin.handlers.admin import router as admin_router
+from Telegram.modules.admin.handlers.files import router as file_router
 from Telegram.modules.user.handlers.user import router as user_router
 
 # dp.include_router(router)
 dp.include_router(admin_router)
+dp.include_router(file_router)
 dp.include_router(user_router)
 dp.include_router(start_router)
 dp.include_router(echo_router)
