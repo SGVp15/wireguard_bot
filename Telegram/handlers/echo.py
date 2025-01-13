62245d9da83a6384fdfc679ba4380d0d95eef642
SGVp15 <phosphorusp15@mail.ru>
2025-01-13 23:34:45 +0300

    log

--- Telegram/handlers/echo.py
+++ Telegram/handlers/echo.py
@@ -1,13 +1,17 @@
+import logging
+
 from aiogram import types, F, Router
 from aiogram.fsm.context import FSMContext
 
 from Telegram.loader import bot
+from utils.log import log
 
 router = Router()
 
 
 @router.callback_query()
 async def echo(callback_query: types.callback_query, state: FSMContext):
+    log.info(f'{callback_query=}\n{state=}')
     await bot.send_message(
         chat_id=callback_query.from_user.id,
         text=f'Не понимаю, что это значит.\n'
@@ -26,6 +30,7 @@ async def echo(message: types.Message):
 
 @router.message()
 async def echo(message: types.Message):
+    log.info(f'{message=}')
     await message.reply(
         f'[{message.text}] --- Не понимаю, что это значит.'
         'Воспользуйтесь командой /help',
