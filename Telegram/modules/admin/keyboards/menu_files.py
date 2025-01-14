530a9aa041ba256510f2ce9ae7f82521d5da2fa1
SGVp15 <phosphorusp15@mail.ru>
2025-01-14 13:57:14 +0300

    refactoring

--- Telegram/modules/admin/keyboards/menu_files.py
+++ Telegram/modules/admin/keyboards/menu_files.py
@@ -30,7 +30,7 @@ def builder_config_list_files_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(text='🔙 Назад', callback_data=CALL_BACK_DATA.menu_users)
     for config_file in config_files:
-        builder.button(text=f'⏬{config_file}', callback_data=DOWNLOAD_CONFIG_FILE(name=config_file).pack())
+        builder.button(text=f'⏬ {config_file}', callback_data=DOWNLOAD_CONFIG_FILE(name=config_file).pack())
     builder.button(text='🔙 Назад', callback_data=CALL_BACK_DATA.menu_users)
     builder.adjust(1)
     return builder.as_markup()
