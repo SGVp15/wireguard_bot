180655e2f9757924eec2baa7cfe23daeb6de929c
SGVp15 <phosphorusp15@mail.ru>
2025-01-14 11:01:03 +0300

    log

--- Telegram/modules/admin/keyboards/menu_admin.py
+++ Telegram/modules/admin/keyboards/menu_admin.py
@@ -2,6 +2,8 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from Telegram.Call_Back_Data import CALL_BACK_DATA
 
+button_menu_admin = InlineKeyboardButton(text='🔙 Назад', callback_data=CALL_BACK_DATA.menu_admin),
+
 k_menu_admin = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='🌟 Перезагрузить службу wireguard',
                           callback_data=CALL_BACK_DATA.menu_restart_service_wg), ],
@@ -19,13 +21,13 @@ k_menu_admin = InlineKeyboardMarkup(inline_keyboard=[
 k_menu_restart_service = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='🛑 Все верно перезагрузить службу wireguard!',
                           callback_data=CALL_BACK_DATA.restart_service_wg_ok), ],
-    [InlineKeyboardButton(text='🔙 Назад', callback_data=CALL_BACK_DATA.menu_admin), ],
+    [button_menu_admin],
 
 ])
 
 k_menu_reboot_server = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='🛑 Все верно перезагрузить Server!',
                           callback_data=CALL_BACK_DATA.reboot_server_ok), ],
-    [InlineKeyboardButton(text='🔙 Назад', callback_data=CALL_BACK_DATA.menu_admin), ],
+    [button_menu_admin],
 
 ])
