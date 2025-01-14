432595a7ff03b005196f21a17f11c79090e6ac25
SGVp15 <phosphorusp15@mail.ru>
2025-01-14 10:33:27 +0300

    log

--- Telegram/modules/admin/keyboards/menu_files.py
+++ Telegram/modules/admin/keyboards/menu_files.py
@@ -1,15 +1,14 @@
 import os
 
 from aiogram.filters.callback_data import CallbackData
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from Telegram.Call_Back_Data import CALL_BACK_DATA
-from config import PATH_CONFIG, PATH_QR
+from config import PATH_CONFIG
 
 
 class DOWNLOAD_CONFIG_FILE(CallbackData, prefix='download_conf_file'):
-    name:str
+    name: str
 
 
 class DOWNLOAD_QR_FILE(CallbackData, prefix='download_qr_file'):
@@ -27,14 +26,10 @@ class DELETE_QR_FILE(CallbackData, prefix='delete_qr_file'):
 
 
 def builder_config_list_files_keyboard() -> InlineKeyboardMarkup:
-    out_buttons = []
     config_files = os.listdir(PATH_CONFIG)
     config_files = sorted(config_files)
-    qrcodes = os.listdir(PATH_QR)
-
     builder = InlineKeyboardBuilder()
     for config_file in config_files:
-        builder.button(text=f'⏬{config_file}',
-                       callback_data=DOWNLOAD_CONFIG_FILE(name=config_file).pack())
+        builder.button(text=f'⏬{config_file}', callback_data=DOWNLOAD_CONFIG_FILE(name=config_file).pack())
     builder.adjust(1)
     return builder.as_markup()
