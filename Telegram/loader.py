ac75b9fc87283c496b7b87822126f27cfed0e512
SGVp15 <phosphorusp15@mail.ru>
2025-01-15 20:43:12 +0300

    VERSION = '1.6'

--- Telegram/loader.py
+++ Telegram/loader.py
@@ -1,13 +1,13 @@
 import asyncio
 
 from aiogram import Bot, Router, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from Telegram import config
 
-bot = Bot(token=config.BOT_TOKEN)
+bot = Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 router = Router(name=__name__)
 loop = asyncio.get_event_loop()
-storage = MemoryStorage()
-dp = Dispatcher(storage=storage, parse_mode=ParseMode.HTML)
+dp = Dispatcher(storage=MemoryStorage(), parse_mode=ParseMode.HTML)
