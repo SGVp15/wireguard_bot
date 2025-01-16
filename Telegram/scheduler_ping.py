8f157e90ef997a198f3e1a500144dd608b216c1d
SGVp15 <phosphorusp15@mail.ru>
2025-01-16 20:36:16 +0300

    ping host

--- Telegram/scheduler_ping.py
+++ Telegram/scheduler_ping.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from datetime import datetime
 
 from Telegram.utils.admin import send_message_to_admins
 
@@ -14,7 +15,7 @@ async def ping_host(host: str = '195.91.139.50'):
         response = os.system(command)
 
         if response == 0 and down:
-            await send_message_to_admins(text=f"{host} is UP!")
+            await send_message_to_admins(text=f"{datetime.now()} - {host} is UP!")
             print(f"{host} is UP!")
             down = []
 
@@ -22,7 +23,7 @@ async def ping_host(host: str = '195.91.139.50'):
             down.append(True)
 
         if len(down) == 2 and all(down):
-            await send_message_to_admins(text=f"{host} is DOWN!")
+            await send_message_to_admins(text=f"{datetime.now()} - {host} is DOWN!")
             print(f"{host} is DOWN!")
 
         await asyncio.sleep(10)
