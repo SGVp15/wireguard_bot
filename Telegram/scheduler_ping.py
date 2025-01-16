47426a29dee200e2b9ecd35af55970d6950250db
SGVp15 <phosphorusp15@mail.ru>
2025-01-16 20:30:10 +0300

    ping host

--- Telegram/scheduler_ping.py
+++ Telegram/scheduler_ping.py
@@ -1,21 +1,18 @@
 import asyncio
-import subprocess
 
 import ping3
-from pythonping import ping
 
 from Telegram.utils.admin import send_message_to_admins
 
 
-async def ping_ip(ip: str = '195.91.139.50'):
-    await asyncio.sleep(10)
+async def ping_host(host: str = '195.91.139.50'):
     down = []
     while True:
-        command = f'ping -c 1 -w2 {ip} '  # > /dev/null 2>&1'
+        command = f'ping -c 1 -w2 {host} '  # > /dev/null 2>&1'
 
         # process = await asyncio.create_subprocess_shell(command, stdout=asyncio.subprocess.PIPE)
         # stdout, stderr = await process.communicate()
-        response = ping3.ping(ip)
+        response = ping3.ping(host)
         # if response is None:
         # process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         # output, error = process.communicate(timeout=1)
@@ -29,15 +26,15 @@ async def ping_ip(ip: str = '195.91.139.50'):
         # response = stdout
         # print(response)
         if response is None and down:
-            await send_message_to_admins(text=f"{ip} is UP!")
-            print(f"{ip} is UP!")
+            await send_message_to_admins(text=f"{host} is UP!")
+            print(f"{host} is UP!")
             down = []
 
         if response != 0:
             down.append(True)
 
         if len(down) == 2 and all(down):
-            await send_message_to_admins(text=f"{ip} is DOWN!")
-            print(f"{ip} is DOWN!")
+            await send_message_to_admins(text=f"{host} is DOWN!")
+            print(f"{host} is DOWN!")
 
         await asyncio.sleep(3)
--- main.py
+++ main.py
@@ -1,39 +1,27 @@
 import asyncio
-import signal
 
 from Telegram.main import start_bot
-from Telegram.scheduler_ping import ping_ip
+from Telegram.scheduler_ping import ping_host
 from utils.log import log
 from wireguard.wireguard_class import WIREGUARD
-
-
-async def ping_ip_with_cancellation(cancellation_event):
-    """Continuously pings the IP address until cancellation is requested."""
-    while True:
-        try:
-            await asyncio.wait_for(ping_ip(), timeout=20)  # Ping every 60 seconds
-        except asyncio.TimeoutError:
-            log.warning("Ping timed out.")
-        except asyncio.CancelledError:
-            log.info("Ping loop cancelled.")
-            break
-        await asyncio.sleep(1)  # Brief delay between pings
 
 
 async def main():
     tasks = [
         start_bot(),
-        ping_ip_with_cancellation(asyncio.Event()),  # Pass a cancellation event
+        ping_host(),
     ]
-    await asyncio.gather(*tasks)
+    # await asyncio.gather(*tasks)
+
+
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    for task in pending:
+        task.cancel()
+    print("Первая задача завершена, остальные отменены")
+
 
 
 if __name__ == '__main__':
     WIREGUARD().create_all_qrcodes()
     log.info('wireguard-bot start')
-
-    # Graceful shutdown on Ctrl+C
-    cancellation_event = asyncio.Event()
-    signal.signal(signal.SIGINT, lambda sig, frame: cancellation_event.set())
-
-    asyncio.run(main(), cancellation_event=cancellation_event)
+    asyncio.run(main())
