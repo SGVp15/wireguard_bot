1186b91b0bddadca26bae6919a38ca68c36e9cb7
SGVp15 <phosphorusp15@mail.ru>
2025-01-16 20:12:56 +0300

    ping server on  subprocess.Popen

--- Telegram/scheduler_ping.py
+++ Telegram/scheduler_ping.py
@@ -1,6 +1,7 @@
 import asyncio
 import subprocess
 
+import ping3
 from pythonping import ping
 
 from Telegram.utils.admin import send_message_to_admins
@@ -12,22 +13,22 @@ async def ping_ip(ip: str = '195.91.139.50'):
     while True:
         command = f'ping -c 1 -w2 {ip} '  # > /dev/null 2>&1'
 
-        process = await asyncio.create_subprocess_shell(command, stdout=asyncio.subprocess.PIPE)
-        stdout, stderr = await process.communicate()
-
+        # process = await asyncio.create_subprocess_shell(command, stdout=asyncio.subprocess.PIPE)
+        # stdout, stderr = await process.communicate()
+        response = ping3.ping(ip)
+        # if response is None:
         # process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         # output, error = process.communicate(timeout=1)
 
-        if process.returncode == 0:
-            print("Команда выполнена успешно")
-        else:
-            print("Ошибка при выполнении команды:", stderr)
-        process.kill()
-
-
-        response = stdout
-        print(response)
-        if response == 0 and down:
+        # if process.returncode == 0:
+        #     print("Команда выполнена успешно")
+        # else:
+        #     print("Ошибка при выполнении команды:", stderr)
+        #
+        #
+        # response = stdout
+        # print(response)
+        if response is None and down:
             await send_message_to_admins(text=f"{ip} is UP!")
             print(f"{ip} is UP!")
             down = []
