f0234986a5db2a416cd2f100fd992ee879e59f0b
SGVp15 <phosphorusp15@mail.ru>
2025-01-20 20:09:41 +0300

    DELETE_CONFIG_FILE

--- wireguard/user_config.py
+++ wireguard/user_config.py
@@ -8,27 +8,30 @@ class UserConfig:
     def __init__(self, file_name: str):
         self.name = file_name.replace('.conf', '')
 
-        self.qr_file = os.path.join(PATH_QR, f'{self.name}.png')
-        self.public_key = os.path.join(PATH_KEYS, f'{self.name}_public.key')
-        self.private_key = os.path.join(PATH_KEYS, f'{self.name}_private.key')
-        self.config_file = os.path.join(PATH_CONFIG, f'{self.name}.conf')
+        self.path_qr_file = os.path.join(PATH_QR, f'{self.name}.png')
+        self.path_public_key = os.path.join(PATH_KEYS, f'{self.name}_public.key')
+        self.path_private_key = os.path.join(PATH_KEYS, f'{self.name}_private.key')
+        self.path_config_file = os.path.join(PATH_CONFIG, f'{self.name}.conf')
+
         self.address = ''
         self.public_key = ''
-        if os.path.exists(self.config_file):
-            with open(self.config_file, 'r') as f:
+        if os.path.exists(self.path_config_file):
+            with open(self.path_config_file, 'r') as f:
                 s = f.read()
                 self.address = re.findall(r'Address\s*=\s*(\d+\.\d+\.\d+\.\d+/\d+)', s)[0].strip()
-        if os.path.exists(self.public_key):
-            with open(self.public_key, 'r') as f:
+        if os.path.exists(self.path_public_key):
+            with open(self.path_public_key, 'r') as f:
                 self.public_key = f.read().strip()
 
     def rename_conf(self, new_name):
-        for path in (self.private_key, self.public_key, self.config_file, self.qr_file):
+        for path in (self.path_private_key, self.path_public_key, self.path_config_file, self.path_qr_file):
             dist = os.path.join(os.path.dirname(path), os.path.basename(path).replace(self.name, new_name))
             os.rename(path, dist)
 
     def delete_conf(self):
-        for path in (self.private_key, self.public_key, self.config_file, self.qr_file):
-            # os.makedirs(os.path.join(os.path.dirname(path), 'trash'), exist_ok=True)
+        for path in (self.path_private_key, self.path_public_key, self.path_config_file, self.path_qr_file):
             dist = os.path.join(os.path.dirname(path), 'trash', os.path.basename(path))
-            os.renames(path, dist)
+            try:
+                os.renames(path, dist)
+            except FileNotFoundError:
+                print(path)
