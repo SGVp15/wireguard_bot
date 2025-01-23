edc1e6d7f4495d49db0842a8e277408305681738
SGVp15 <phosphorusp15@mail.ru>
2025-01-24 00:19:05 +0300

    VERSION = '1.7.5'

--- Telegram/modules/user/handlers/userConfig.py
+++ Telegram/modules/user/handlers/userConfig.py
@@ -44,14 +44,12 @@ async def create_user_config(callback_query: CallbackQuery, state: FSMContext):
                        )
 async def rename_user_config(callback_query: CallbackQuery, state: FSMContext):
     data = await state.get_data()
-    name = data.get('name')
-    user_config = UserConfig(name)
-    new_name = data.get('new_name')
-    if user_config.rename_conf(new_name):
+    user_config: UserConfig = data.get('user_config')
+    if user_config.rename_conf(user_config.new_name):
         log.info('rename_user {user} -> {new_name}')
         await state.clear()
         await callback_query.message.edit_text(
-            text=f'Rename: {name} -> {new_name} ok',
+            text=f'Rename: {user_config.name} -> {user_config.new_name} ok',
             reply_markup=k_back_to_menu_users
         )
     else:
@@ -59,6 +57,7 @@ async def rename_user_config(callback_query: CallbackQuery, state: FSMContext):
             text=f'Такое имя уже существует',
             reply_markup=k_back_to_menu_users
         )
+
 
 @router.callback_query(DELETE_CONFIG_FILE.filter())
 async def delete_user_config(callback_query: CallbackQuery,
