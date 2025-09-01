4baf38207485d863376c36422366b1bb1b2bf2d1
SGVp15 <phosphorusp15@mail.ru>
2025-09-01 18:09:29 +0300

    VER 2.2 abc xray

--- Telegram/loader.py
+++ Telegram/loader.py
@@ -8,6 +8,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from Telegram import config
 
 bot = Bot(token=str(config.BOT_TOKEN), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+print('-' * 30, config.BOT_TOKEN)
 router = Router(name=__name__)
 loop = asyncio.get_event_loop()
 dp = Dispatcher(storage=MemoryStorage(), parse_mode=ParseMode.HTML)
